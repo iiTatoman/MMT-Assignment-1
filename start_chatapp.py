e759b74803da67f16b507ed7b2cdd473b88d6958
Khang Nguyen <khanghyper1610@gmail.com>
2026-04-20 22:58:47 +0700

    edit

--- start_chatapp.py
+++ start_chatapp.py
@@ -1,11 +1,3 @@
-#
-# Copyright (C) 2026 pdnguyen of HCMC University of Technology VNU-HCM.
-# All rights reserved.
-# This file is part of the CO3093/CO3094 course.
-#
-# AsynapRous release
-#
-
 """
 start_chatapp
 ~~~~~~~~~~~~~
